8e1414df9b7e0f6d67dbc6fe87196bf69994f909
DimasNandaHerlambang <55137751+DimasNandaHerlambang@users.noreply.github.com>
2021-10-01 02:14:44 +0700

    vidio #6 Assets
    Menyelsaikan tugas vidio 6 tentang Assets

---
+++ app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php 
+
+define('BASEURL', 'http://localhost/prakweb_phpmvc_193040040/public');
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,6 @@
-
+<div class="container">
 	<h1>About Me</h1>
+	<img src="<?= BASEURL; ?>/img/FOTO NEW 001.jpg" alt="Dimas" width="200" class="rounded-circle">
 	<p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur'];?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+
+</div>
