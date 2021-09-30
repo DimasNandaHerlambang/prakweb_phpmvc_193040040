4be2f4ee9e53739b521dec3c579a8c76f26c343e
DimasNandaHerlambang <55137751+DimasNandaHerlambang@users.noreply.github.com>
2021-09-30 19:38:16 +0700

    tugas vidio 4 CONTROLLER

---
+++ app/controllers/About.php
@@ -0,0 +1,12 @@
+<?php 
+
+class About {
+	public funtion index($nama = 'Dimas', $pekerjaan = 'Mahasiswa')
+	{
+		echo "Halo, nama saya $nama, Saya adalah seorang $pekerjaan";
+	}
+	public function page()
+	{
+		echo 'About/page';
+	}
+}
